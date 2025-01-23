47510e031c078bd7ff42685f4990724ec38f936c
karl <sylveuskarl54@gmail.com>
2025-01-23 17:05:11 -0500

    new changes

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,33 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        addItem: (state, action) => {
+  const { name, image, cost } = action.payload;
+  const existingItem = state.items.find(item => item.name === name);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    state.items.push({ name, image, cost, quantity: 1 });
+  }
+},
     
     },
     removeItem: (state, action) => {
+        const { name, quantity } = action.payload;
+            state.items = state.items.filter(item => item.name !== name);
+            state.numOfItems -= quantity;
+
+            // Just to be sure... I hate negative numbers
+            if (state.numOfItems < 0) {
+                state.numOfItems = 0;
+            } 
     },
     updateQuantity: (state, action) => {
-
+const { name, quantity } = action.payload;
+const itemToUpdate = state.items.find(item => item.name === name);
+if (itemToUpdate) {
+  itemToUpdate.quantity = quantity;
+}
     
     },
   },
