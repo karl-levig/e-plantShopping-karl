4d618955a6860ab8792ea66895d7a335e0104df6
karl levig <sylveuskarl54@gmail.com>
2025-01-26 11:37:00 -0500

    newup

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -16,40 +16,40 @@ function ProductList() {
             category: "Air Purifying Plants",
             plants: [
                 {
-                    name: "Snake Plant",
-                    image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
-                    description: "Produces oxygen at night, improving air quality.",
-                    cost: "$15"
-                },
-                {
-                    name: "Spider Plant",
-                    image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
-                    description: "Filters formaldehyde and xylene from the air.",
-                    cost: "$12"
-                },
-                {
-                    name: "Peace Lily",
-                    image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
-                    description: "Removes mold spores and purifies the air.",
+                    name: "Bamboo Palm",
+                    image: "https://cdn.pixabay.com/photo/2020/01/30/04/27/palm-4804295_1280.jpg",
+                    description: "Removes carbon monoxide and formaldehyde,Thrives in indirect light.",
                     cost: "$18"
                 },
                 {
-                    name: "Boston Fern",
-                    image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
-                    description: "Adds humidity to the air and removes toxins.",
-                    cost: "$20"
-                },
-                {
-                    name: "Rubber Plant",
-                    image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
-                    description: "Easy to care for and effective at removing toxins.",
+                    name: " Dracaena Marginata",
+                    image: "https://cdn.pixabay.com/photo/2017/08/13/15/37/green-plants-2637613_1280.jpg",
+                    description: "Eliminates benzene, xylene, and formaldehyde.",
                     cost: "$17"
                 },
                 {
-                    name: "Aloe Vera",
-                    image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
-                    description: "Purifies the air and has healing properties for skin.",
-                    cost: "$14"
+                    name: " Golden Pothos",
+                    image: "https://cdn.pixabay.com/photo/2018/11/15/10/32/plants-3816945_1280.jpg",
+                    description: "Removes formaldehyde and other toxins.",
+                    cost: "$22"
+                },
+                {
+                    name: "Kimberly Queen Fern",
+                    image: "https://cdn.pixabay.com/photo/2015/04/10/00/40/green-715535_1280.jpg",
+                    description: "Excellent at increasing humidity and purifying air.",
+                    cost: "$24"
+                },
+                {
+                    name: " Gerbera Daisy",
+                    image: "https://cdn.pixabay.com/photo/2023/06/22/17/05/flowers-8081925_1280.jpg",
+                    description: "Bright blooms add color to interiors.",
+                    cost: "$20"
+                },
+                {
+                    name: "Fiddle Leaf Fig ",
+                    image: "https://cdn.pixabay.com/photo/2019/08/26/15/22/fig-tree-4432033_1280.jpg",
+                    description: "Filters air while being a stylish decor piece.",
+                    cost: "$17"
                 }
             ]
         },
@@ -57,10 +57,10 @@ function ProductList() {
             category: "Aromatic Fragrant Plants",
             plants: [
                 {
-                    name: "Lavender",
-                    image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                    description: "Calming scent, used in aromatherapy.",
-                    cost: "$20"
+                    name: "Gardenia",
+                    image: "https://cdn.pixabay.com/photo/2020/04/27/02/41/gardenia-5097886_1280.jpg",
+                    description: "Thrives in warm, humid climates.",
+                    cost: "$24"
                 },
                 {
                     name: "Jasmine",
